2cf48a68dac2124b2414f7890ea03ca6c1c54748
Francisco Sant'Anna <francisco.santanna@gmail.com>
2023-08-01 17:58:52 -0300

    ...

--- src/main/kotlin/Aux.kt
+++ src/main/kotlin/Aux.kt
@@ -28,17 +28,6 @@ fun Expr.is_lval (): Boolean {
     }
 }
 
-fun Expr.base (): Expr.Acc {
-    return when (this) {
-        is Expr.Acc   -> this
-        is Expr.Index -> this.col.base()
-        else -> {
-            println(this)
-            TODO()
-        }
-    }
-}
-
 fun String.tag2c (): String {
     return this
         .drop(1)
